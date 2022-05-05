35f303bc46ca5ff56ddac679ff32e6ea1126ba1c
Lex <totallabyss@gmail.com>
2022-05-05 12:13:29 +0700

    add wireframe classes and methods

---
+++ TelegramPAI/PaiBot.cs
@@ -0,0 +1,30 @@
+﻿namespace TelegramPAI
+{
+	internal class PaiBot
+	{
+		public PaiBot()
+		{
+		}
+
+		private Dictionary<string, Category> Categories { get; set; }; // мапа для хранения категорий и ссылок
+
+		private Commands commands; //класс для исполнения команд
+
+		private static ITelegramBotClient bot = new TelegramBotClient("TOKEN"); //сохранения токена бота
+
+		public static void Start()
+		{
+			//запуск бота, получение обновлений
+		}
+		public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+		{
+			//для ошибок
+		}
+
+		public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+		{
+			//обработка новых входящих сообщений от пользователя
+			//выполнение основного кода программы, все команды, сохранение и вывод категорий происходит тут
+		}
+	}
+}
--- TelegramPAI/Program.cs
+++ TelegramPAI/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using TelegramPAI;
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+PaiBot.Start(); //переход в метод запуска бота и старта основной программы
